ca69795bdd89d75ed06018da99db5226c10d9a19
rockinrubal <rockinrubal@gmail.com>
2020-09-20 15:33:40 +0530

    update dist

---
+++ dist/matrix_rwx_pii-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+     name='matrix_rwx_pii',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     scripts=['matrix']                  # The name of your scipt, and also the command you'll be using for calling it
+)
